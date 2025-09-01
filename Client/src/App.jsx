532946a51b4c8c9988d2ddad9962a4ac7443d7dd
Bikash-design-lab <vmodi5425@gmail.com>
2025-09-01 18:45:45 +0530

    "install react"

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p className='text-3xl underline text-center'  >Hello world</p>
+    </div>
+  )
+}
+
+export default App
